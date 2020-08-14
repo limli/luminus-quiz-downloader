9fb04bcb8643d7770e86884119d6566e333df9ef
limli <limlirandom@gmail.com>
2020-08-14 13:20:32 +0800

    add files

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # luminus-quiz-downloader
+For downloading quizes on luminus, since printing the page doesn't work.
+
+Instructions:
+1. Go to the luminus quiz page you would like to print
+2. Open the developer console (f12 on Chrome browser)
+3. Copy and paste the js script `quizdownload.js` into the console. A `myquiz.html` file should be downloaded.
+4. Open the downloaded `myquiz.html` file. You can print this file to pdf now.
---
+++ quizdownload.js
@@ -0,0 +1,32 @@
+qns = document.getElementsByClassName("question-view")
+
+function download(filename, text) {
+  var element = document.createElement('a');
+  element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+  element.setAttribute('download', filename);
+
+  element.style.display = 'none';
+  document.body.appendChild(element);
+
+  element.click();
+
+  document.body.removeChild(element);
+}
+
+style = `
+<style>
+.option-content{display:flex;align-items: center;}
+</style>
+`
+str = style;
+for(let q of qns){
+  const clonedQ = q.cloneNode(true);
+  for(let box of clonedQ.querySelectorAll('input')){
+    if((box.type == "checkbox" || box.type == "radio") && box.checked){
+      box.setAttribute('checked', true);
+    }
+  }
+  str += clonedQ.innerHTML;
+}
+
+download("myquiz.html", str);
